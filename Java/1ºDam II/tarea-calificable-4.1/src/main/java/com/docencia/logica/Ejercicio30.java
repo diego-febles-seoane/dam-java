7c3822501f5ed3d253f20d8a059286b1d37fcf49
Diego Febles Seoane <diegofebless@gmail.com>
2026-02-26 18:40:14 +0000

    Actulizacion 26/02/2026

--- Java/1ºDam II/tarea-calificable-4.1/src/main/java/com/docencia/logica/Ejercicio30.java
+++ Java/1ºDam II/tarea-calificable-4.1/src/main/java/com/docencia/logica/Ejercicio30.java
@@ -1,11 +1,40 @@
 package com.docencia.logica;
 
+import java.util.Arrays;
+
 public class Ejercicio30 {
   /**
    * Subset sum (existe subconjunto)
    * TODO: Implementar usando lógica tradicional (iterativo).
    */
   public static boolean existeSubconjuntoSuma(int[] arreglo, int objetivo) {
-    throw new UnsupportedOperationException("Pendiente de implementar");
+    if (arreglo == null || arreglo.length == 0) {
+      return false;
+    }
+    if (objetivo == 0) {
+      return true;
+    }
+    int resultado = 0;
+    for (int i = 0; i < arreglo.length; i++) {
+      resultado += arreglo[i];
+    }
+    if (resultado == objetivo) {
+      return true;
+    }
+    int posicion = 0;
+    int valorInicial = arreglo[posicion];
+    for (int i = posicion + 1; i < arreglo.length; i++) {
+      int[] subConjunto = Arrays.copyOfRange(arreglo, posicion, arreglo.length);
+      for (int j = 0; j < subConjunto.length; j++) {
+        int valor = subConjunto[posicion];
+        int valorEncontrado = valor + valorInicial;
+        if (valorEncontrado == objetivo) {
+          return true;
+        }
+      }
+      posicion++;
+
+    }
+    return false;
   }
 }
